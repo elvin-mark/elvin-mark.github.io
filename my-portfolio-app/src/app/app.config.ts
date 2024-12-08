50acd84d5fe992faa7bcc8264f10d588e2ab5b85
elvin-mark <munoz.vega.em@gmail.com>
2024-12-08 14:03:55 +0900

    updates: - adding missing images for projects and profile picture - adding blog contents. all future blogs to be added will be through markdown files

--- my-portfolio-app/src/app/app.config.ts
+++ my-portfolio-app/src/app/app.config.ts
@@ -3,7 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideMarkdown } from 'ngx-markdown';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { MarkdownModule } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideMarkdown(),
+    provideHttpClient(),
+    MarkdownModule.forRoot({ loader: HttpClient }).ngModule,
+  ],
 };
--- my-portfolio-app/src/app/app.routes.ts
+++ my-portfolio-app/src/app/app.routes.ts
@@ -5,12 +5,19 @@ import { BlogsComponent } from './blogs/blogs.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ContactComponent } from './contact/contact.component';
+import { PapersComponent } from './papers/papers.component';
+import { BlogContentComponent } from './blogs/components/blog-content/blog-content.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'experience', component: ExperienceComponent },
+  { path: 'papers', component: PapersComponent },
   { path: 'blogs', component: BlogsComponent },
+  {
+    path: 'blog',
+    component: BlogContentComponent,
+  },
   { path: 'contact', component: ContactComponent },
 ];
